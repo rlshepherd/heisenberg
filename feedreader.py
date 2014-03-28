dd4b1ce10f0cef80b042d780e86d2eba4e43dccd
Russell Shepherd <russell.l.shepherd@gmail.com>
2014-03-28 17:37:04 +0800

    Check if article already exists.

--- feedreader.py
+++ feedreader.py
@@ -5,19 +5,21 @@ import time
 def GetURL(rssEntry):
   return rssEntry.feedburner_origlink
 
-def GetSavedArticles():
-  pass
+def CheckForExisiting(rssEntry):
+  if r.db("heisenberg").table("articles").get(GetURL(rssEntry)).run():
+    return True
+  else:
+    return False
 
 def SaveArticle(rssEntry):
   r.db("heisenberg").table("articles").insert({
     "url": GetURL(rssEntry),
-    "savedate": time.strftime("%x %X"),
+    "savedate": Time.now.utc,
     "shares":[]
     }).run()
 
 r.connect('localhost', 28015).repl()
 
-savedArticles = GetSavedArticles()
-
 for rssEntry in fp.parse('http://feeds.feedburner.com/rappler/').entries:
+  if not CheckForExisting(rssEntry):
     SaveArticle(rssEntry)
