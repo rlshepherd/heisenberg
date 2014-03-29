13d795183402b079f127ef0cc1b7d316fcb073e6
Russell Shepherd <russell.l.shepherd@gmail.com>
2014-03-29 20:38:59 +0800

    PEP8 formatting

--- updatecounts.py
+++ updatecounts.py
@@ -33,83 +33,91 @@ import urllib2
 import datetime
 from docopt import docopt
 
+
 def Twitter(url):
-  '''Return total shares on Twitter for a url.'''
-  response = urllib2.urlopen(
-    'http://cdn.api.twitter.com/1/urls/count.json?url=%s' % url)
-  data = json.loads(response.read())
-  if 'count' in data.keys():
-    return data['count']
-  else:
-    return 0
+    '''Return total shares on Twitter for a url.'''
+    response = urllib2.urlopen(
+        'http://cdn.api.twitter.com/1/urls/count.json?url=%s' % url)
+    data = json.loads(response.read())
+    if 'count' in data.keys():
+        return data['count']
+    else:
+        return 0
+
 
 def Facebook(url):
-  '''Return total shares on Facebook for a url.'''
-  response = urllib2.urlopen(
-    'http://graph.facebook.com/?id=%s' % url)
-  data = json.loads(response.read())
-  if 'shares' in data.keys():
-    return data['shares']
-  else:
-    return 0
+    '''Return total shares on Facebook for a url.'''
+    response = urllib2.urlopen(
+        'http://graph.facebook.com/?id=%s' % url)
+    data = json.loads(response.read())
+    if 'shares' in data.keys():
+        return data['shares']
+    else:
+        return 0
+
 
 def TotalShares(url, platform):
-  '''Return total shares for a given url and platform from platform api'''
-  if platform == "Twitter":
-    return Twitter(url)
-  elif platform == "Facebook":
-    return Facebook(url)
+    '''Return total shares for a given url and platform from platform api'''
+    if platform == "Twitter":
+        return Twitter(url)
+    elif platform == "Facebook":
+        return Facebook(url)
+
 
 def PrevShares(url, platform):
-  '''Return previous shares for a given url and platform from rethinkdb.'''
-  selection = r.db('heisenberg').table('articles').filter({
-    "url": url,
-    "platform": platform,
-    })
-  return json.loads(selection)
+    '''Return previous shares for a given url and platform from rethinkdb.'''
+    selection = r.db('heisenberg').table('articles').filter({
+                                                            "url": url,
+                                                            "platform": platform,
+                                                            })
+    return json.loads(selection)
+
 
 def NewShares(url, platform):
-  '''Get shares since last count for a url and platform'''
-  return TotalShares(url, platform) - PrevShares(url, platform)
+    '''Get shares since last count for a url and platform'''
+    return TotalShares(url, platform) - PrevShares(url, platform)
+
 
 def UpdateShares(url, platform):
-  '''Save count of new shares to rethinkdb.'''
-  r.db('heisenberg').table('articles').filter({'url': url}).update({
-      'shares': r.row['shares'].append({
-        'count':52,
-        'timestamp':r.now(),
-        'platform':platform
-    })
-  }).run()
+    '''Save count of new shares to rethinkdb.'''
+    r.db('heisenberg').table('articles').filter({'url': url}).update({
+        'shares': r.row['shares'].append({
+                                         'count': 52,
+        'timestamp': r.now(),
+            'platform': platform
+        })
+    }).run()
+
 
 def GetSelection(lowerBound, upperBound):
-  now = datetime.datetime.utcnow()
-  y = now - datetime.timedelta(lowerBound)
-  x = now - datetime.timedelta(upperBound)
-  selection = r.db('heisenberg').table('articles').filter(
-    r.row['savedate'].during(
-      r.time(x.year, x.month, x.day, x.hour, x.minute, x.second, "Z"),
-      r.time(y.year, y.month, y.day, y.hour, y.minute, y.second, "Z"))
-  ).run()
+    now = datetime.datetime.utcnow()
+    y = now - datetime.timedelta(lowerBound)
+    x = now - datetime.timedelta(upperBound)
+    selection = r.db('heisenberg').table('articles').filter(
+        r.row['savedate'].during(
+            r.time(x.year, x.month, x.day, x.hour, x.minute, x.second, "Z"),
+            r.time(y.year, y.month, y.day, y.hour, y.minute, y.second, "Z"))
+    ).run()
 
-  return([list(selection)])
+    return([list(selection)])
+
 
 def UpdateRange(lowerBound, upperBound):
-  '''Update all articles older than lowerBound days old, and younger than
-     upperBound days.
-  '''
-  selection = GetSelection(lowerBound, upperBound)
-  for article in selection:
-    UpdateShares(article.url, 'Twitter')
-    UpdateShares(article.url, 'Facebook')
+    '''Update all articles older than lowerBound days old, and younger than
+       upperBound days.
+    '''
+    selection = GetSelection(lowerBound, upperBound)
+    for article in selection:
+        UpdateShares(article.url, 'Twitter')
+        UpdateShares(article.url, 'Facebook')
 
 if __name__ == '__main__':
-  arguments = docopt(__doc__, version='Heisenberg v0.1')
+    arguments = docopt(__doc__, version='Heisenberg v0.1')
 
-  if arguments['lowerBound'] < arguments['upperBound']:
-    lowerBound = arguments['lowerBound']
-    upperBound = arguments['upperBound']
-  else:
-    raise exception('lowerBound must be smaller than upperBound!')
-  r.connect('localhost', 28015).repl()
-  UpdateRange(lowerBound, upperBound)
+    if arguments['lowerBound'] < arguments['upperBound']:
+        lowerBound = arguments['lowerBound']
+        upperBound = arguments['upperBound']
+    else:
+        raise exception('lowerBound must be smaller than upperBound!')
+    r.connect('localhost', 28015).repl()
+    UpdateRange(lowerBound, upperBound)
